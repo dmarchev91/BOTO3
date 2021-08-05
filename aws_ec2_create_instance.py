e133c7d4ccb09bca20e95203151a560c7f0baf09
dmarchev91 <79225410+dmarchev91@users.noreply.github.com>
2021-08-05 17:12:51 +0300

    Add files via upload

--- aws_ec2_create_instance.py
+++ aws_ec2_create_instance.py
@@ -14,4 +14,24 @@ def create_ec2_instance():
    except Exception as e:
        print(e)
 
-create_ec2_instance()
+
+def describe_ec2_instance():
+    try:
+        print("Describing EC2-Instance")
+        resource_ec2 = boto3.client("ec2")
+        print(resource_ec2.describe_instances())
+    except Exception as e:
+        print(e)
+
+def terminate_ec2_instance():
+    try:
+        print("Terminating EC2-Instance")
+        instance_id = describe_ec2_instance()
+        resource_ec2 = boto3.client("ec2")
+        print(resource_ec2.terminate_instances(InstanceIds=[instance_id]))
+    except Exception as e:
+        print(e)
+
+#describe_ec2_instance()
+#create_ec2_instance()
+terminate_ec2_instance()
